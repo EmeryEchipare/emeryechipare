220a4369c691812e293ff359082770934deed460
Emery Echipare <Emery.Echipare@gmail.com>
2025-11-20 23:35:43 -0700

    Fix CORS to allow DELETE method and Authorization header

--- worker-api/src/index.js
+++ worker-api/src/index.js
@@ -19,8 +19,8 @@ function getCorsHeaders(origin) {
   
   return {
     'Access-Control-Allow-Origin': isAllowed ? origin : allowedOrigins[0],
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Max-Age': '86400',
   };
 }
